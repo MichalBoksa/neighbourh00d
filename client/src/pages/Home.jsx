010097846ec4abafe7a5b4fd0935bff9ab5551ea
unknown <ul0249604@edu.uni.lodz.eu>
2023-04-25 00:21:26 +0200

    home logo deploying

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,15 +3,23 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import ProductsMainPage from '../components/ProductsMainPage'
 import Slider from '../components/Slider'
+import logo from '../images/logo.jpg';
+
+
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
-        <Slider/>
-        <ProductsMainPage/>
-        <Footer/>
+    
+    <div style={{backgroundColor:"black",height:"100vh"}}> 
+
+        <a href="https://google.com" class="button" style = {{marginLeft:"1",MarginRight:"1"}}> <img src={logo} border="0" ></img></a>
     </div>
+    // <div>
+    //     <Navbar/>
+    //     <Slider/>
+    //     <ProductsMainPage/>
+    //     <Footer/>
+    // </div>
   )
 }
 
